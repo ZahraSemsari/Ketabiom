f073464296741960de0fce6c0b3a4322940a7178
ZahraSemsari <semsarizahra64@gmail.com>
2026-07-04 19:32:24 +0330

    Correct a mistake in package

--- ketabiom_project/settings.py
+++ ketabiom_project/settings.py
@@ -41,21 +41,26 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'accounts',
+
+    'rest_framework',
     'drf_yasg',
-    "corsheaders",
+    'corsheaders',
+
+    'accounts',
     'books',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+
+    "corsheaders.middleware.CorsMiddleware",
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    "corsheaders.middleware.CorsMiddleware" ,
 ]
 
 ROOT_URLCONF = 'ketabiom_project.urls'
